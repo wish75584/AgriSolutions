f43b3586f96d6148d892254ac130c0f0cc83605b
wish75584 <37246279+wish75584@users.noreply.github.com>
2019-05-09 15:52:10 +0530

    Create ApiClient.java

---
+++ ApiClient.java
@@ -0,0 +1,28 @@
+package com.stubborn.android.agrisolutions;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient {
+
+    private static final String BASE_URL = "http://half-witted-transis.000webhostapp.com/";
+    private static Retrofit retrofit;
+
+    public static Retrofit getAppClient(){
+        if(retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+            Gson gson = new GsonBuilder()
+                    .setLenient()
+                    .create();
+
+        }
+        return retrofit;
+
+    }
+}
